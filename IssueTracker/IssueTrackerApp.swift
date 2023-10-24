a0455e261ef415715a511e48d1301b36dbeef084
Joel Storr <joel.storr@ewe.net>
2023-10-24 19:39:46 +0200

    Initial Commit

---
+++ IssueTracker/IssueTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IssueTrackerApp.swift
+//  IssueTracker
+//
+//  Created by Joel Storr on 24.10.23.
+//
+
+import SwiftUI
+
+@main
+struct IssueTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
