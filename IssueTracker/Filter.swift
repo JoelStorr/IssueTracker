d76d0787e7babc30e30f1570475dbfd16567811b
Joel Storr <joel.storr@ewe.net>
2023-10-25 09:54:57 +0200

    Added Sorting to issue list

--- IssueTracker/Filter.swift
+++ IssueTracker/Filter.swift
@@ -12,12 +12,12 @@ struct Filter: Identifiable, Hashable{
     var id: UUID
     var name: String
     var icon: String
-    var modificationDate = Date.distantPast
+    var minModificationDate = Date.distantPast
     var tag: Tag?
     
     
     static var all = Filter(id: UUID(), name: "All Issues", icon: "tray")
-    static var recent = Filter(id: UUID(), name: "Recent Issues", icon: "clock", modificationDate: .now.addingTimeInterval(86400 * -7))
+    static var recent = Filter(id: UUID(), name: "Recent Issues", icon: "clock", minModificationDate: .now.addingTimeInterval(86400 * -7))
     
     
     func hash(into hasher: inout Hasher) {
