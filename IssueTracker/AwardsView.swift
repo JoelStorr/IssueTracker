efb3c0fe2f313be07aacb387bcca3f80e52dd26c
Joel Storr <joel.storr@ewe.net>
2023-10-26 10:08:05 +0200

    Added UI for awards sheet

--- IssueTracker/AwardsView.swift
+++ IssueTracker/AwardsView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct AwardsView: View {
+    
+    var columns: [GridItem] {
+        [GridItem(.adaptive(minimum: 100, maximum: 100))]
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ScrollView{
+                LazyVGrid(columns: columns) {
+                    ForEach(Award.allAwards){ award in
+                        Button{
+                            
+                        } label: {
+                            Image(systemName: award.image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                                .frame(width: 100,height: 100)
+                                .foregroundStyle(.secondary.opacity(0.5))
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Awards")
+        }
     }
 }
 
