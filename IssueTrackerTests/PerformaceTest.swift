3c185db7f563e963bc7adccfc891787453a252e7
Joel Storr <joel.storr@ewe.net>
2023-10-27 17:16:45 +0200

    Added Performance Test

---
+++ IssueTrackerTests/PerformaceTest.swift
@@ -0,0 +1,27 @@
+//
+//  PerformaceTest.swift
+//  IssueTrackerTests
+//
+//  Created by Joel Storr on 27.10.23.
+//
+
+import XCTest
+@testable import IssueTracker
+
+final class PerformaceTest: BaseTestCase {
+
+    func testAwardCalculationPerformance(){
+        for _ in 1...100 {
+            dataController.createSampleData()
+        }
+        
+        let awards = Array( repeating: Award.allAwards, count: 25).joined()
+        XCTAssertEqual(awards.count, 500, "This checks if the Awards count is constant. Change this if you add awards")
+        
+        measure{
+            _ = awards.filter(dataController.hasEarned)
+        }
+        
+    }
+
+}
