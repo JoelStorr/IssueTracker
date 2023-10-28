6fd4f030b5c0c8ccf0f8d42f537ae3922812e43c
Joel Storr <joel.storr@ewe.net>
2023-10-28 15:59:08 +0200

    Added custom haptics when closing a Issue

--- IssueTracker/Editing/IssueViewToolbar.swift
+++ IssueTracker/Editing/IssueViewToolbar.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct IssueViewToolbar: View {
 
     @EnvironmentObject var dataController: DataController
     @ObservedObject var issue: Issue
+
+    @State private var engine = try? CHHapticEngine()
 
     var openCloseButtonText: LocalizedStringKey {
         issue.completed ? "Re-open Issue" : "Close Issue"
@@ -24,18 +27,64 @@ struct IssueViewToolbar: View {
                 Label("Copy Issue Title", systemImage: "doc.on.doc")
             }
 
-            Button {
-                issue.completed.toggle()
-                dataController.save()
-            } label: {
+            Button(action: toggleCompleted) {
                 Label(
                     openCloseButtonText,
                     systemImage: "bubble.left.and.exclamationmark.bubble.right"
                 )
             }
+            // IOS 17 only
+//            .sensoryFeedback(trigger: issue.completed) { oldValue, newValue in
+//                if newValue {
+//                    .success
+//                } else {
+//                    nil
+//                }
+//            }
 
         } label: {
           Label("Actions", systemImage: "ellipsis.circle")
+        }
+    }
+
+    func toggleCompleted() {
+        issue.completed.toggle()
+        dataController.save()
+        if issue.completed {
+            do {
+                try engine?.start()
+
+                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+
+                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+
+                let parameter = CHHapticParameterCurve(
+                    parameterID: .hapticIntensityControl,
+                    controlPoints: [start, end],
+                    relativeTime: 0
+                )
+
+                let event1 = CHHapticEvent(
+                    eventType: .hapticTransient,
+                    parameters: [intensity, sharpness],
+                    relativeTime: 0
+                )
+
+                let  event2 = CHHapticEvent(
+                    eventType: .hapticContinuous,
+                    parameters: [sharpness, intensity],
+                    relativeTime: 0.125,
+                    duration: 1
+                )
+
+                let pattern = try CHHapticPattern(events: [event1, event2], parameterCurves: [parameter])
+                let player = try engine?.makePlayer(with: pattern)
+                try player?.start(atTime: 0)
+            } catch {
+                // playing haptick didn't work but thats okay
+            }
         }
     }
 }
